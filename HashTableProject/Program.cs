7f77b170cc4c059cdaa5b5f402a01fd8b66895ff
Rajeshwarikota <kotarajeswari003.com>
2023-02-15 14:18:05 +0530

    [Rajeshwari] Adding Hash Table frequency in paragraph

--- HashTableProject/Program.cs
+++ HashTableProject/Program.cs
@@ -11,28 +11,48 @@ namespace HashTableProject
         static void Main(string[] args)
         {
             Console.WriteLine("...Hash Table...");
-
-               string paragraph = "To be or not to be";
-                CountNumbOfOccurence(paragraph);
-                Console.ReadLine();
-            }
-            public static void CountNumbOfOccurence(string paragraph)
+            bool flag = true;
+            while (flag)
             {
-                MyMapNode<string, int> hashTable = new MyMapNode<string, int>(6);
-
-                string[] words = paragraph.Split(' ');
-
-                foreach (string word in words)
+                Console.WriteLine("Please Choose \n1.Sentence Frequency \n2.Paragraph Frequency \n22.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
                 {
-                    if (hashTable.Exists(word.ToLower()))
-                        hashTable.Add(word.ToLower(), hashTable.Get(word.ToLower()) + 1);
-                    else
-                        hashTable.Add(word.ToLower(), 1); //to,1 
+                    case 1:
+                        string paragraph = "To be or not to be";
+                        CountNumbOfOccurence(paragraph);
+                        break;
+                    case 2:
+                        string paragraph2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                        CountNumbOfOccurence(paragraph2);
+                        break;
+                    case 22:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct number ! ");
+                        break;
                 }
-                Console.WriteLine("Displaying Freuency of HashTable");
-                hashTable.Display();
-            }
            
+        }
+    }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTable = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTable.Exists(word.ToLower()))
+                    hashTable.Add(word.ToLower(), hashTable.Get(word.ToLower()) + 1);
+                else
+                    hashTable.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("Displaying Freuency of HashTable");
+            hashTable.Display();
+        }
+       
     }
 }
 
