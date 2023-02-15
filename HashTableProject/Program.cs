6e7a9ae37016f2dc489303dc8463d2d02766aee2
Rajeshwarikota <kotarajeswari003.com>
2023-02-15 13:48:15 +0530

    [Rajeshwari] Adding Hash Table frequency in sentence

--- HashTableProject/Program.cs
+++ HashTableProject/Program.cs
@@ -11,7 +11,28 @@ namespace HashTableProject
         static void Main(string[] args)
         {
             Console.WriteLine("...Hash Table...");
-            Console.ReadLine();
-        }
+
+               string paragraph = "To be or not to be";
+                CountNumbOfOccurence(paragraph);
+                Console.ReadLine();
+            }
+            public static void CountNumbOfOccurence(string paragraph)
+            {
+                MyMapNode<string, int> hashTable = new MyMapNode<string, int>(6);
+
+                string[] words = paragraph.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (hashTable.Exists(word.ToLower()))
+                        hashTable.Add(word.ToLower(), hashTable.Get(word.ToLower()) + 1);
+                    else
+                        hashTable.Add(word.ToLower(), 1); //to,1 
+                }
+                Console.WriteLine("Displaying Freuency of HashTable");
+                hashTable.Display();
+            }
+           
     }
 }
+
